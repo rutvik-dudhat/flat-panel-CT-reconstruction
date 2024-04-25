54c22b709c368b9b729129112110f4b8a4041c70
Rutvik Dudhat <rutvik.dudhat@fau.de>
2024-04-25 16:19:52 +0200

    Added ex1

--- EX-1/Grid.py
+++ EX-1/Grid.py
@@ -1,9 +1,10 @@
 import numpy as np
-
+import interpolate as ip
 
 # import your necessary libraries by yourself
 
 # Fill in the missing part of the following functions, class attributes
+
 
 class Grid:
 
@@ -11,38 +12,41 @@ class Grid:
         self.height = height
         self.width = width
         self.spacing = spacing
-        self.origin = (0, 0)
-        self.buffer =np.zeros((height, width))
+        self.origin = [-0.5 * (width - 1.0) * spacing[0], -0.5 * (height - 1.0) * spacing[1]]
+        # oworld = [ox, oy] = [-0.5(N0-1)s0, -0.5(N1-1)s1]
+        # N0 = width, N1 = height, S0 = spacing[0], S1 = spacing[1]
+        # to make the origin at the center of the grid
+        self.buffer =np.empty((height, width))
 
     def set_at_index(self, i, j, val):
         self.buffer[i,j] = val
-
+        # set buffer value at the index (i,j)
 
     def get_at_index(self, i, j):
         return self.buffer[i,j]
-
+        # get buffer value at the index (i,j)
 
     def get_at_physical(self, x, y):
-        i,j =self.physical_to_index(x,y)
-        return self.get_at_index(i,j)
+        i,j=self.physical_to_index(x,y)
+        return ip.interpolate(self,i,j)
+        # convert physical to to index coordinates and then interpolate
 
 
     def index_to_physical(self, i, j):
-        #return i*self.spacing, j*self.spacing
-        x=self.origin[0]+i*self.spacing
-        y=self.origin[1]+j*self.spacing
+        x=self.origin[0]+i*self.spacing[0]
+        y=self.origin[1]+j*self.spacing[1]
         return x,y
+    #convert grid (i,j) to pysical (x,y)
 
 
     def physical_to_index(self, x, y):
-        i=int((x-self.origin[0])/self.spacing)
-        j=int((y-self.origin[1])/self.spacing)
-        return i,j
-
+        return np.array([((x - self.origin[0]) / self.spacing[0]),
+                         ((y - self.origin[1]) / self.spacing[1])])
+#convert physical (x,y) to grid (i,j)
 
     def get_size(self):
         return self.height, self.width
-
+#return the size of the grid
 
     def set_origin(self, origin):
         self.origin = origin
